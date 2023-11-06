9b2070f13c110aa790519a5834c89fee2af809bf
Mert Emir <48881002+mertemr@users.noreply.github.com>
2023-11-06 14:16:09 +0300

    Update scoop_install.ps1

--- scoop_install.ps1
+++ scoop_install.ps1
@@ -1,6 +1,60 @@
-Invoke-RestMethod get.scoop.sh | Invoke-Expression
+Set-ExecutionPolicy Bypass -Scope CurrentUser -Force;
 
-scoop install git
-scoop bucket add extras
-scoop install neovim gcc busybox sudo gsudo 7zip fx
-scoop cleanup *; scoop cache rm -a
+Invoke-RestMethod get.scoop.sh | Invoke-Expression;
+scoop install git 7zip;
+
+@(
+  "extras",
+  "nirsoft",
+  "sysinternals"
+) | ForEach-Object { "scoop bucket add $_" } | Invoke-Expression;
+
+scoop update;
+
+scoop install aria2;
+
+scoop config aria2-enabled true;
+scoop config aria2-warning-enabled false;
+
+scoop config aria2-split 16;
+scoop config aria2-max-connection-per-server 16;
+
+scoop install gsudo;
+sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1;
+
+@(
+  "adb",
+  "autoruns",
+  "busybox",
+  "dark",
+  "dasel",
+  "everything",
+  "ffmpeg",
+  "gcc",
+  "innounp",
+  "lxrunoffline",
+  "neofetch",
+  "neovim",
+  "nodejs",
+  "oh-my-posh",
+  "posh-git",
+  "processhacker",
+  "python",
+  "rammap",
+  "scoop-completion",
+  "scoop-search",
+  "upx"
+) | ForEach-Object { "scoop install $_" } | Invoke-Expression -ErrorAction Continue;
+
+@(
+  "everything\current\install-context.reg",
+  "7zip\current\install-context.reg",
+  "git\current\install-file-associations.reg",
+  "everything\current\install-context.reg",
+  "python\current\install-pep-514.reg",
+) | ForEach-Object { reg import "C:\Users\$env:USERNAME\scoop\apps\$_" } | Invoke-Expression;
+
+scoop cleanup *;
+scoop cache rm -a;
+
+scoop alias add upgrade "scoop update -f; scoop update -a; scoop cleanup *; scoop cache rm -a; scoop checkup; scoop status;"
